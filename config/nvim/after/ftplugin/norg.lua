3dff590b48df416702160d8acdec1d3f1a3f5426
Crag <ogeidca49@gmail.com>
2023-08-28 13:20:33 -0600

    improbe eww, change commit

--- config/nvim/after/ftplugin/norg.lua
+++ config/nvim/after/ftplugin/norg.lua
@@ -1,6 +1,5 @@
-vim.opt_local.tw = 100
 vim.opt_local.concealcursor = "n"
 -- vim.opt_local.concealcursor = ""
 vim.opt_local.conceallevel = 2
 -- vim.opt_local.wrap = true
-vim.opt_local.textwidth = 180
+vim.opt_local.textwidth = 150
