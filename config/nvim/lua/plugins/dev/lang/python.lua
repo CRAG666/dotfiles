0ccf9c50f8475461ed2ee3da70c4f1988428b508
Crag <ogeidca49@gmail.com>
2025-07-20 16:49:13 -0600

    fix(Upda python):

--- config/nvim/lua/plugins/dev/lang/python.lua
+++ config/nvim/lua/plugins/dev/lang/python.lua
@@ -66,23 +66,4 @@ return {
       },
     },
   },
-  { -- directly open ipynb files as quarto docuements
-    -- and convert back behind the scenes
-    'GCBallesteros/jupytext.nvim',
-    ft = 'jupyter',
-    opts = {
-      custom_language_formatting = {
-        python = {
-          extension = 'qmd',
-          style = 'quarto',
-          force_ft = 'quarto',
-        },
-        r = {
-          extension = 'qmd',
-          style = 'quarto',
-          force_ft = 'quarto',
-        },
-      },
-    },
-  },
 }
