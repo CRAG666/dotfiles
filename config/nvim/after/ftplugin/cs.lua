8a6ec5a0a302f59425fc1139590cd33239d8b3e2
Crag <ogeidca49@gmail.com>
2025-09-19 15:57:43 -0600

    neovim marked

---
+++ config/nvim/after/ftplugin/cs.lua
@@ -0,0 +1,14 @@
+set = vim.bo
+set.shiftwidth = 4
+set.softtabstop = 4
+set.expandtab = true
+vim.g.mason = { 'csharp-language-server', 'omnisharp' }
+vim.g.ts = { 'c_sharp', 'jsonnet' }
+
+-- Set ; to end line
+vim.keymap.set(
+  'n',
+  '<leader>;',
+  '<esc>mzA;<esc>`z',
+  { noremap = true, silent = true }
+)
---
+++ config/nvim/after/ftplugin/lua.lua
@@ -0,0 +1,8 @@
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.textwidth = 106
+vim.bo.expandtab = true
+vim.bo.autoindent = true
+vim.g.mason = { 'stylua', 'shfmt', 'lua-language-server', 'efm', 'luacheck' }
+vim.g.ts = { 'lua', 'luadoc', 'luap', 'luau' }
